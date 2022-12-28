1bdc37ea0894636f7c058464435ef6bab0a3a6ce
endreo.cba@gmail.com <Thays*8171>
2022-12-28 13:04:26 -0400

    atualizado

---
+++ lib/view/calculadora.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Calculadora extends StatefulWidget {
+  const Calculadora({super.key});
+
+  @override
+  State<Calculadora> createState() => _CalculadoraState();
+}
+
+class _CalculadoraState extends State<Calculadora> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
